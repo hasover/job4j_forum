748720b2343ad08560110a8907c889d0f3e399a6
hasover <hasover@gmail.com>
2022-01-20 00:06:35 +0300

    images

--- src/main/java/ru/job4j/forum/controller/LoginController.java
+++ src/main/java/ru/job4j/forum/controller/LoginController.java
@@ -1,27 +1,17 @@
 package ru.job4j.forum.controller;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.job4j.forum.model.User;
-import ru.job4j.forum.service.AccessService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
-    private AccessService accessService;
-
-    public LoginController(AccessService accessService) {
-        this.accessService = accessService;
-    }
 
     @GetMapping("/login")
     public String loginPage(@RequestParam(value = "error", required = false) String error,
